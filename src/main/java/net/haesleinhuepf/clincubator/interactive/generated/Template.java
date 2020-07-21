ca590c885c4b746441d7a841cff5df935890b824
Robert Haase <haesleinhuepf@users.noreply.github.com>
2020-07-21 17:48:28 +0200

    cleanup template plugin

--- src/main/java/net/haesleinhuepf/clincubator/interactive/generated/Template.java
+++ src/main/java/net/haesleinhuepf/clincubator/interactive/generated/Template.java
@@ -1,25 +1,14 @@
 package net.haesleinhuepf.clincubator.interactive.generated;
 
-import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
-import net.haesleinhuepf.clij2.plugins.Mean3DBox;
-import net.haesleinhuepf.clij2.plugins.MeanZProjection;
-import net.haesleinhuepf.clijx.CLIJx;
 import net.haesleinhuepf.clincubator.AbstractIncubatorPlugin;
-import net.haesleinhuepf.clincubator.interactive.processing.BackgroundSubtraction;
-import net.haesleinhuepf.clincubator.interactive.projections.PopularIntensityProjection;
-import net.haesleinhuepf.clincubator.interactive.transform.CylinderProjection;
-import net.haesleinhuepf.clincubator.interactive.transform.RigidTransform3D;
-import net.haesleinhuepf.clincubator.interactive.transform.SphereProjection;
 import net.haesleinhuepf.clincubator.utilities.SuggestedPlugin;
-import net.haesleinhuepf.spimcat.io.CLIJxVirtualStack;
 import org.scijava.plugin.Plugin;
-
 
 //@Plugin(type = SuggestedPlugin.class)
 public class Template extends AbstractIncubatorPlugin implements SuggestedPlugin {
 
     public Template() {
-        super(new Mean3DBox());
+        super(new net.haesleinhuepf.clij2.plugins.Mean3DBox());
     }
 
     public Class[] suggestedNextSteps() {
