76811407e30c1cfcd782e339bf9745d48a0c58de
haesleinhuepf <rhaase@mpi-cbg.de>
2020-08-26 10:43:39 +0200

    made Weka work

---
+++ src/main/java/net/haesleinhuepf/clijx/assistant/interactive/handcrafted/BinaryWekaPixelClassifier.java
@@ -0,0 +1,123 @@
+package net.haesleinhuepf.clijx.assistant.interactive.handcrafted;
+
+import ij.IJ;
+import ij.gui.GenericDialog;
+import ij.gui.Toolbar;
+import ij.plugin.frame.RoiManager;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.plugins.AutoThresholderImageJ1;
+import net.haesleinhuepf.clijx.CLIJx;
+import net.haesleinhuepf.clijx.assistant.AbstractAssistantGUIPlugin;
+import net.haesleinhuepf.clijx.assistant.optimize.BinaryAnnotationTool;
+import net.haesleinhuepf.clijx.assistant.optimize.OptimizationUtilities;
+import net.haesleinhuepf.clijx.assistant.services.AssistantGUIPlugin;
+import net.haesleinhuepf.clijx.assistant.utilities.AssistantUtilities;
+import net.haesleinhuepf.clijx.assistant.utilities.IJLogger;
+import net.haesleinhuepf.clijx.assistant.utilities.Logger;
+import net.haesleinhuepf.clijx.weka.GenerateFeatureStack;
+import net.haesleinhuepf.clijx.weka.TrainWekaModel;
+import net.haesleinhuepf.clijx.weka.TrainWekaModelWithOptions;
+import net.haesleinhuepf.spimcat.io.CLIJxVirtualStack;
+import org.scijava.plugin.Plugin;
+import org.scijava.util.VersionUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Plugin(type = AssistantGUIPlugin.class)
+public class BinaryWekaPixelClassifier extends AbstractAssistantGUIPlugin {
+
+    GenericDialog dialog;
+
+    public BinaryWekaPixelClassifier() {
+        super(new net.haesleinhuepf.clijx.plugins.BinaryWekaPixelClassifier());
+    }
+
+    @Override
+    protected GenericDialog buildNonModalDialog(Frame parent) {
+        GenericDialog gd = super.buildNonModalDialog(parent);
+        dialog = gd;
+
+        gd.addToSameRow();
+        {
+            Panel panel = new Panel();
+            Button button = new Button("File...");
+            button.addActionListener((a) -> {
+                String file = IJ.getFilePath("Model location");
+                if (file.length() > 0) {
+                    ((TextField) gd.getStringFields().get(1)).setText(file);
+                }
+            });
+            panel.add(button);
+            gd.addPanel(panel);
+        }
+
+        gd.addNumericField("Number of trees", 200, 0);
+        gd.addNumericField("Number of features", 2, 0);
+        gd.addNumericField("Max depth", 0, 0);
+
+        {
+            Panel panel = new Panel();
+            Button button = new Button("Train");
+            button.addActionListener((a) -> {
+                train(new IJLogger());
+            });
+            panel.add(button);
+            gd.addPanel(panel);
+        }
+        gd.addMessage("Note: This only works for 2D images for now.");
+
+        for (int i = 0; i < 2; i++) {
+            TextField t = ((TextField) gd.getStringFields().get(i));
+            t.setMinimumSize(new Dimension(200, t.getHeight()));
+        }
+
+        return gd;
+    }
+
+    private void train(Logger logger) {
+        logger.log("Train Weka");
+        logger.log("----------");
+
+        CLIJ2 clij2 = CLIJx.getInstance();
+        logger.log("GPU: " + clij2.getGPUName() + " (OCLv: " + clij2.getOpenCLVersion() + ", AssistantV: " + VersionUtils.getVersion(this.getClass()) + ")");
+
+        // -------------------------------------------------------------------------------------------------------------
+        // determine ground truth
+        RoiManager rm = RoiManager.getRoiManager();
+        if (rm.getCount() == 0) {
+            IJ.log("Please define reference ROIs in the ROI Manager.\n\n" +
+                    "These ROIs should have names starting with 'p' for positive and 'n' for negative.\n\n" +
+                    "The just activated annotation tool can help you with that.");
+            Toolbar.addPlugInTool(new BinaryAnnotationTool());
+            return;
+        }
+        ClearCLBuffer ground_truth = OptimizationUtilities.makeGroundTruth(clij2, my_target.getWidth(), my_target.getHeight(), my_target.getNSlices(), rm);
+
+        ClearCLBuffer[] pushed = CLIJxVirtualStack.imagePlusToBuffer(my_source);
+        ClearCLBuffer input = pushed[my_source.getC() - 1];
+
+        String feature_definitions = ((TextField)dialog.getStringFields().get(0)).getText();
+        String model_filename = ((TextField)dialog.getStringFields().get(1)).getText();
+
+        int num_trees = (int)Double.parseDouble(((TextField)dialog.getNumericFields().get(0)).getText());
+        int num_features = (int)Double.parseDouble(((TextField)dialog.getNumericFields().get(0)).getText());
+        int max_depth = (int)Double.parseDouble(((TextField)dialog.getNumericFields().get(0)).getText());
+
+        ClearCLBuffer featureStack = GenerateFeatureStack.generateFeatureStack(clij2, input, feature_definitions);
+
+        TrainWekaModelWithOptions.trainWekaModelWithOptions(clij2, featureStack, ground_truth, model_filename, num_trees, num_features, max_depth);
+
+        cleanup(my_source, pushed);
+
+        ground_truth.close();
+        featureStack.close();
+
+        logger.log("Model saved to " + model_filename);
+        setTargetInvalid();
+        logger.log("Bye.");
+    }
+
+}
--- src/main/java/net/haesleinhuepf/clijx/plugins/BinaryWekaPixelClassifier.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/BinaryWekaPixelClassifier.java
@@ -7,12 +7,15 @@ import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
 import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.utilities.IsCategorized;
 import net.haesleinhuepf.clijx.weka.ApplyWekaModel;
 import net.haesleinhuepf.clijx.weka.GenerateFeatureStack;
 import org.scijava.plugin.Plugin;
 
-//@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_applyBinaryWekaPixelClassifier")
-public class ApplyBinaryWekaPixelClassifier extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+import java.io.File;
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_binaryWekaPixelClassifier")
+public class BinaryWekaPixelClassifier extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized {
 
     @Override
     public String getParameterHelpText() {
@@ -32,14 +35,25 @@ public class ApplyBinaryWekaPixelClassifier extends AbstractCLIJ2Plugin implemen
         String features = (String) args[2];
         String model_filename = (String) args[3];
 
-        return applyBinaryWekaPixelClassifier(getCLIJ2(), input, output, features, model_filename);
+        return binaryWekaPixelClassifier(getCLIJ2(), input, output, features, model_filename);
     }
 
-    public static boolean applyBinaryWekaPixelClassifier(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output, String features, String model_filename)
+    public static boolean binaryWekaPixelClassifier(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output, String features, String model_filename)
     {
+        if (!new File(model_filename).exists()) {
+            clij2.set(output, 0);
+            System.out.println("Model " + model_filename + " not found. Cancelling binaryWekaPixelClassifier.");
+            return true;
+        }
         ClearCLBuffer featureStack = GenerateFeatureStack.generateFeatureStack(clij2, input, features);
-        ApplyWekaModel.applyWekaModel(clij2, featureStack, output, model_filename);
+
+        ClearCLBuffer temp = clij2.create(output);
+
+        ApplyWekaModel.applyWekaModel(clij2, featureStack, temp, model_filename);
         featureStack.close();
+
+        clij2.addImageAndScalar(temp, output, -1);
+        temp.close();
 
         return true;
     }
@@ -54,5 +68,10 @@ public class ApplyBinaryWekaPixelClassifier extends AbstractCLIJ2Plugin implemen
     @Override
     public String getAvailableForDimensions() {
         return "2D";
+    }
+
+    @Override
+    public String getCategories() {
+        return "Binary,Segmentation";
     }
 }
