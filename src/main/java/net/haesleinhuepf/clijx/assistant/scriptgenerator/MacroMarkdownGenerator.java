14352e4e66e58b1dba2f46f69be0ea055b4cbe1e
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-05 09:11:50 +0200

    multi-source support for macro generators

--- src/main/java/net/haesleinhuepf/clijx/assistant/scriptgenerator/MacroMarkdownGenerator.java
+++ src/main/java/net/haesleinhuepf/clijx/assistant/scriptgenerator/MacroMarkdownGenerator.java
@@ -1,6 +1,8 @@
 package net.haesleinhuepf.clijx.assistant.scriptgenerator;
 
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clijx.assistant.annotation.ClassificationClass;
 import net.haesleinhuepf.clijx.assistant.services.AssistantGUIPlugin;
 import net.haesleinhuepf.clijx.assistant.utilities.AssistantUtilities;
 
@@ -20,24 +22,31 @@ public class MacroMarkdownGenerator extends MacroGenerator {
         String program = "/*\n" +
                 "## " + AssistantUtilities.niceNameWithoutDimShape(plugin.getName()) + "\n";
 
-        for (int s = 0; s < plugin.getNumberOfSources(); s++) {
-            program = program +
-                    "" + image1s[s] + ": \"" + plugin.getSource(s).getTitle() + "\";\n";
-        }
+        //for (int s = 0; s < plugin.getNumberOfSources(); s++) {
+        //    program = program +
+        //            "" + image1s[s] + ": \"" + plugin.getSource(s).getTitle() + "\";\n";
+        //}
         program = program +
-                "" + image2 + ": \"" + plugin.getTarget().getTitle() + "\"\n" +
+        //        "" + image2 + ": \"" + plugin.getTarget().getTitle() + "\"\n" +
                 "*/\n";
 
         String call = "";
 
         String[] parameters = clijMacroPlugin.getParameterHelpText().split(",");
-        for (int i = 2; i < parameters.length; i++) {
+        for (int i = 0; i < parameters.length; i++) {
             String temp[] = parameters[i].trim().split(" ");
             String name = temp[temp.length - 1];
-            call = call + ", " + name;
-            program = program + name + " = " + objectToString(plugin.getArgs()[i]) + ";\n";
+            if (i > 0) {
+                call = call + ", ";
+            }
+            if (plugin.getArgs()[i] instanceof ClearCLBuffer || plugin.getArgs()[i] instanceof ClearCLBuffer[]) {
+                call = call + objectToString(plugin.getArgs()[i]);
+            } else {
+                call = call + name;
+                program = program + name + " = " + objectToString(plugin.getArgs()[i]) + "; //\n";
+            }
         }
-        program = program + methodName + "(" + namesToCommaSeparated(image1s) + ", " + image2 + call + ");\n";
+        program = program + methodName + "(" + call + ");\n";
         //program = program + "Ext.CLIJ2_pull(" + image2 + "); // consider removing this line if you don't need to see that image\n";
 
         return program;
