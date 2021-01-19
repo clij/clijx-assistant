1bc108c4efd21c9465b940bae5d41b08d8f68c97
haesleinhuepf <robert_haase@gmx.de>
2021-01-19 18:02:02 +0100

    regenerated clesperanto compatibility lists and API

--- src/main/java/net/clesperanto/javaprototype/SnakeInterface.java
+++ src/main/java/net/clesperanto/javaprototype/SnakeInterface.java
@@ -500,6 +500,8 @@ import net.haesleinhuepf.clijx.plugins.VisualizeOutlinesOnOriginal;
 import net.haesleinhuepf.clijx.plugins.FlagLabelsOnEdges;
 import net.haesleinhuepf.clijx.plugins.MaskedVoronoiLabeling;
 import net.haesleinhuepf.clijx.plugins.PullToResultsTableColumn;
+import net.haesleinhuepf.clijx.plugins.KMeansLabelClusterer;
+import net.haesleinhuepf.clijx.plugins.ModeOfTouchingNeighbors;
 // this is generated code. See src/test/java/net/haesleinhuepf/clijx/codegenerator for details
 abstract class SnakeInterface extends CommonAPI {
    static CLIJ getCLIJ() {
@@ -8128,5 +8130,33 @@ abstract class SnakeInterface extends CommonAPI {
         return arg1;
     }
 
+
+    // net.haesleinhuepf.clijx.plugins.KMeansLabelClusterer
+    //----------------------------------------------------
+    /**
+     * Applies K-Means clustering to an image and a corresponding label map. 
+     * 
+     * See also: https://commons.apache.org/proper/commons-math/javadocs/api-3.6/org/apache/commons/math3/ml/clustering/KMeansPlusPlusClusterer.html
+     * Make sure that the handed over feature list is the same used while training the model.
+     * The neighbor_radius specifies a correction step which allows to use a region where the mode of 
+     * classification results (the most popular class) will be determined after clustering.
+     */
+    public static ClearCLBuffer k_means_label_clusterer(ClearCLBuffer arg1, ClearCLBuffer arg2, ClearCLBuffer arg3, String arg4, String arg5, double arg6, double arg7, boolean arg8) {
+        KMeansLabelClusterer.kMeansLabelClusterer(getCLIJ2(), arg1, arg2, arg3, arg4, arg5, new Double (arg6).intValue(), new Double (arg7).intValue(), arg8);
+        return arg3;
+    }
+
+
+    // net.haesleinhuepf.clijx.plugins.ModeOfTouchingNeighbors
+    //----------------------------------------------------
+    /**
+     * Takes a touch matrix and a vector of values to determine the most popular integer value among touching neighbors for every object.
+     * TODO: This only works for values between 0 and 255 for now.
+     */
+    public static ClearCLBuffer mode_of_touching_neighbors(ClearCLBuffer values, ClearCLBuffer touch_matrix, ClearCLBuffer mode_values_destination) {
+        ModeOfTouchingNeighbors.modeOfTouchingNeighbors(getCLIJ2(), values, touch_matrix, mode_values_destination);
+        return mode_values_destination;
+    }
+
 }
-// 540 methods generated.
+// 542 methods generated.
